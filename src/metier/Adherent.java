b0b2ad641d66e64b3796e0618d262f6fa0216819
CaptainJava1991 <carthy@live.fr>
2016-04-06 10:36:23 +0200

    version 1

--- src/metier/Adherent.java
+++ src/metier/Adherent.java
@@ -70,6 +70,7 @@ public class Adherent extends Utilisateur {
 		
 		return nbRetard;
 	}
+	
 	@Override
 	public boolean isConditionsPretAcceptees(){
 		return ((getNbRetards() == 0 && getEmpruntEnCours().size() < nbMaxPrets)? 
